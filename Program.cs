830a5a14f76357e01fec807a067af01c57b68a43
JoshuaHall <joshuahallmail@gmail.com>
2020-04-06 14:24:58 -0500

    In no particular order: Removed unneeded imports Removed unneeded this statements Followed C# naming conventions Made certain methods static that could be made so Moved static methods to the bottom of the class Converted for loops to foreach when able Made most methods accept the more generic IEnumerable<string> where appropriate Made IsHeader easier to read (could arguably make a method for this to make the code more DRY) Updated Fody dependency Added explicit internal/private modifiers

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿using System.Configuration;
-using System.Collections.Specialized;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
-namespace Announcements {
-    static class Program {
+namespace Announcements
+{
+    internal static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        private static void Main()
+        {
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Announcements());
         }
     }
